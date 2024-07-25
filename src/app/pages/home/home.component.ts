3f46afa35e5d00b293d873e6d05bb08582ed76aa
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-07-25 01:34:43 -0500

    update home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, inject, ViewChild } from '@angular/core'
 import { SwiperContainer } from 'swiper/element'
 import { SwiperOptions } from 'swiper/types'
+import { RouterOutlet, RouterLink} from '@angular/router'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterOutlet,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
