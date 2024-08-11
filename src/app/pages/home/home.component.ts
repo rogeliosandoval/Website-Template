727946f696249c091c3488b10a95763e0739ea9e
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-08-10 22:07:16 -0500

    add dynamic meta data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, inject, ViewChild } from '@angular/core'
+import { Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, inject, OnInit, ViewChild } from '@angular/core'
 import { SwiperContainer } from 'swiper/element'
 import { SwiperOptions } from 'swiper/types'
 import { RouterOutlet, RouterLink} from '@angular/router'
+import { Meta, Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-home',
@@ -15,12 +16,22 @@ import { RouterOutlet, RouterLink} from '@angular/router'
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 
-export class Home {
+export class Home implements OnInit {
+  private meta = inject(Meta)
+  private titleService = inject(Title)
   @ViewChild('swiper') swiper!: ElementRef<SwiperContainer>
   public config: SwiperOptions = {
     autoplay: {
       delay: 7000,
       disableOnInteraction: true
     }
+  }
+
+  ngOnInit(): void {
+    this.titleService.setTitle('PVD Solutions | Handyman Services Rhode Island')
+    this.meta.updateTag({ property: 'og:image', content: 'https://pvd-solutions.com/assets/banner.png' }, "property='og:image'")
+    this.meta.updateTag({ property: 'og:title', content: 'Handyman Services Rhode Island | PVD Solutions' }, "property='og:title'")
+    this.meta.updateTag({ property: 'og:url', content: 'https://pvd-solutions.com/' })
+    this.meta.updateTag({ property: 'og:description', content: 'Handyman repairs, comprehensive remodeling and thorough cleaning services.' }, "property='og:description'")
   }
 }
