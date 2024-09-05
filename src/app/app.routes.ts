317547a9bb55feb73699199f2545cf7b160235c5
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-09-05 15:21:50 -0500

    update the doom

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router'
 import { Home } from './pages/home/home.component'
 import { About } from './pages/about/about.component'
 import { Contact } from './pages/contact/contact.component'
+import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy.component'
+import { AccessibilityStatement } from './pages/accessibility-statement/accessibility-statement.component'
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'about', component: About },
-  { path: 'contact', component: Contact }
+  { path: 'contact', component: Contact },
+  { path: 'privacy-policy', component: PrivacyPolicy },
+  { path: 'accessibility-statement', component: AccessibilityStatement },
+  { path: '**', component: Home }
 ]
