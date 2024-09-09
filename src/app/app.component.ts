3df2f9666c9f6f31e053cfac101dba4a21d30343
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-09-09 10:24:55 -0500

    update main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { RouterOutlet, RouterLink } from '@angular/router'
 import { CommonModule } from '@angular/common'
 import { SharedService } from './services/shared.service'
 import { MatIconModule } from '@angular/material/icon'
+import { Meta, Title } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
@@ -19,9 +20,15 @@ import { MatIconModule } from '@angular/material/icon'
 
 export class AppComponent implements OnInit {
   public sharedService = inject(SharedService)
+  private meta = inject(Meta)
+  private titleService = inject(Title)
 
   ngOnInit(): void {
-
+    this.titleService.setTitle('PVD Solutions | Handyman Services Rhode Island')
+    this.meta.updateTag({ property: 'og:image', content: 'https://pvd-solutions.com/assets/banner.png' }, "property='og:image'")
+    this.meta.updateTag({ property: 'og:title', content: 'Handyman Services Rhode Island | PVD Solutions' }, "property='og:title'")
+    this.meta.updateTag({ property: 'og:url', content: 'https://pvd-solutions.com/' })
+    this.meta.updateTag({ property: 'og:description', content: 'Handyman repairs, comprehensive remodeling and thorough cleaning services.' }, "property='og:description'")
   }
 
   public openNav(): void {
