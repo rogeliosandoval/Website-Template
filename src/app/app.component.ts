2c53845d0eff47f28a15e729a84f416a7ef610d1
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-10-06 16:38:09 -0500

    update index, add notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { RouterOutlet, RouterLink } from '@angular/router'
 import { CommonModule } from '@angular/common'
 import { SharedService } from './services/shared.service'
 import { MatIconModule } from '@angular/material/icon'
-import { Meta, Title } from '@angular/platform-browser'
+import { Meta } from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
@@ -21,15 +21,8 @@ import { Meta, Title } from '@angular/platform-browser'
 export class AppComponent implements OnInit {
   public sharedService = inject(SharedService)
   private meta = inject(Meta)
-  private titleService = inject(Title)
 
   public updateMetaTags(): void {
-    this.titleService.setTitle('Chubby Designs | Website Design in San Antonio TX')
-
-    // Standard Meta Tags
-    this.meta.addTag({ name: 'description', content: 'Website design at a realistic cost that is more affordable and straight forward. No money down or contract whatsoever! Contact us today to get started.' })
-
-    // Open Graph Meta Tags
     this.meta.addTag({ property: 'og:title', content: 'Handyman Services Rhode Island | PVD Solutions' })
     this.meta.addTag({ property: 'og:image', content: 'https://pvd-solutions.com/assets/banner.png' })
     this.meta.addTag({ property: 'og:description', content: 'Handyman repairs, comprehensive remodeling and thorough cleaning services.' })
