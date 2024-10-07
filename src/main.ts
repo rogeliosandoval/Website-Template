b0e3622bf22ba077530eaff598cf9690732a38d9
Roger Sandoval <rogelio.g.sandoval@gmail.com>
2024-10-07 18:06:10 -0500

    update robots and main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
-import { register } from 'swiper/element/bundle';
+import { bootstrapApplication } from '@angular/platform-browser'
+import { appConfig } from './app/app.config'
+import { AppComponent } from './app/app.component'
+import { register } from 'swiper/element/bundle'
 
 register()
 
 bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+  .catch((err) => console.error(err))
